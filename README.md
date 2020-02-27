6d77724628a0ffff5ce6d16fa87f5fa979eb7a60
Michael McClintock <mrmcc3@gmail.com>
2020-02-28 09:46:06 +1000

    Generate icons

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+# Heroicons as Hiccup Data
+
+```clojure
+(require '[mrmcc3.heroicons.outline.md :as heroicons])
+
+(heroicons/arrow-circle-right) ;; =>
+[:svg
+ {:fill "none", :viewbox "0 0 24 24", :stroke "currentColor"}
+ [:path
+  {:stroke-linecap "round",
+   :stroke-linejoin "round",
+   :stroke-width "2",
+   :d "M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"}]]
+
+(heroicons/shield-check {:class "h-5"}) ;; =>
+[:svg
+ {:fill "none", :viewbox "0 0 24 24", :stroke "currentColor", :class "h-5"}
+ [:path
+  {:stroke-linecap "round",
+   :stroke-linejoin "round",
+   :stroke-width "2",
+   :d "M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"}]]
+```
+
 # Heroicons
 
 A set of free MIT-licensed high-quality SVG icons for you to use in your web projects.
---
+++ dev/user.clj
@@ -0,0 +1,76 @@
+(ns user
+  (:require
+    [clojure.java.io :as io]
+    [clojure.string :as str]
+    [mrmcc3.heroicons.outline.md :as heroicons]
+    )
+  (:import (org.jsoup Jsoup)))
+
+(defn tag [element]
+  (-> element .tagName keyword))
+
+(defn assoc-attr [m attr]
+  (assoc m (-> attr .getKey keyword) (.getValue attr)))
+
+(defn attr-map [element]
+  (reduce assoc-attr {} (.attributes element)))
+
+(defn element->data [element]
+  (into
+    [(tag element) (attr-map element)]
+    (map element->data)
+    (.children element)))
+
+(defn icon-symbol [file]
+  (-> file .getName
+      (str/split #"\.") first
+      (str/split #"-" 2) second symbol))
+
+(defn icon-form [file]
+  (let [icon (icon-symbol file)
+        arg  'attr-map
+        doc  (str icon " heroicon data. Optionally pass an attr-map to merge with the svg attributes")
+        data (-> file slurp Jsoup/parseBodyFragment
+                 (.getElementsByTag "svg")
+                 first element->data
+                 (update 1 (fn [attrs] `(merge ~attrs ~arg))))]
+    `(defn ~icon ~doc
+       ([] (~icon {}))
+       ([~arg] ~data))))
+
+(defn ns-form [ns]
+  (let [excludes '[filter]]
+    `(ns ~ns (:refer-clojure :exclude ~excludes))))
+
+(defn spit-icons [dir ns]
+  (let [ns-file    (io/file "src-clj" (str (str/replace ns "." "/") ".cljc"))
+        icon-files (file-seq (io/file "dist" dir))]
+    (println "Generating" ns "...")
+    (io/make-parents ns-file)
+    (with-open [writer (io/writer ns-file)]
+      (.write writer (pr-str (ns-form ns)))
+      (.newLine writer)
+      (doseq [icon-file icon-files]
+        (when (.isFile icon-file)
+          (.write writer (pr-str (icon-form icon-file)))
+          (.newLine writer))))))
+
+(defn -main [& args]
+  (spit-icons "outline-md" 'mrmcc3.heroicons.outline.md)
+  (spit-icons "solid-sm" 'mrmcc3.heroicons.solid.sm)
+  (println "done!"))
+
+(comment
+
+  (-main)
+
+  (require '[mrmcc3.heroicons.outline.md :as heroicons])
+
+  (heroicons/arrow-circle-right)
+
+  (heroicons/shield-check {:class "h-5"})
+
+  )
+
+
+
